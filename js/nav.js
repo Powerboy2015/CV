fe0de6393a1350fb30c0bfb4b8772cdb19606065
Powerboy2015 <Zicobacker26@gmail.com>
2024-04-08 16:49:07 +0200

    I hate onedrive
    For some reason onedrive is just the biggest dick in the world and never works with you. I had to redo about 2% of the work but this spaced me out. I am very frustrated with git and onedrive.

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,10 @@
-const navBut = document.querySelector('button');
+const navBut = document.querySelector("#navBut");
 const main = document.querySelector("main");
 const nav = document.querySelector("nav");
 
-
+function toggleNav() {
+    nav.classList.toggle('openNav');
+}
 
 navBut.addEventListener('click',() => {
     nav.classList.toggle('openNav');
