5a5929c0429377b69766d738c968a2c2c7d3d160
Powerboy2015 <Zicobacker26@gmail.com>
2024-04-12 01:51:05 +0200

    github repo list
    after working on this feature way too much, I have managed to create a link between my github repositories and the website. My website will now gather my repository name, desc, lastupdate date, and last commit message. This will be displayed in a div. All the data is saved in cookies that will refresh every 15 minutes in order to keep anonymous requests low and not get blocked out. I have had 3 total reworks of this project and are visible in the repository

---
+++ js/attempt3.js
@@ -0,0 +1,138 @@
+// This has caused me way more pain then it should have. I am so done with life right now.
+class repoObj
+{
+    constructor(name,desc,updated,lastCommit) {
+        this.name = name;
+        this.desc = desc;
+        this.lastUpdate = updated;
+        this.lastCommit = lastCommit;
+    }
+}
+
+const user = 'Powerboy2015';
+const repoContainer = document.querySelector('div.repos');
+
+// fetches user repo list and returns a list of the repo names.
+async function getRepoData(user) {
+    const response = await fetch('https://api.github.com/users/'+user+'/repos');
+    const repoData = await response.json();
+    let Names = [];
+    for(i=0;i != repoData.length; i++) {
+        Names.push(repoData[i]);
+    }
+    return Names;
+}
+
+// requests information about the repo commits.
+async function fetchLastCommit(user,repo){
+    const response = await fetch('https://api.github.com/repos/'+user+'/'+repo+'/commits');
+    const commits = await response.json();
+    return commits[0].commit.message;
+}
+
+
+async function FetchRepoInfo() {
+    const repoNames = await getRepoData(user);
+    const repos = [];
+    for (i=0; i != repoNames.length;i++) {
+        const repo = repoNames[i];
+        const lastCommit = await fetchLastCommit(user,repo.name);
+
+        let time = repo.updated_at.split('T')
+        repos.push(new repoObj(repo.name,repo.description,time[0],lastCommit));
+    }
+    return repos;
+}
+
+function getCookie(name){
+    if(document.cookie != '') {
+            let cookies = document.cookie.split('; ')
+        .find((row) =>
+            row.startsWith(name+'='))
+
+        // if It can't find a row it will it will put 'undefined' into cookies. 
+        // if we can't split it we return undefined.
+        try{
+            let cookieSplit = cookies.split(/=(.*)/s,2);
+            return JSON.parse(cookieSplit[1]);
+        } catch{
+            return undefined;
+        }
+
+        // returns an error if cookies are empty.
+    } else{ 
+        return undefined;
+    };
+}
+
+// creates a function with a duration.
+function createCookie(name,value,duration){
+    document.cookie = name+'='+JSON.stringify(value)+'; max-age='+duration;
+}
+
+// sets the expiration date of a cookie to a date that has already past, making it basically self destruct.
+function deleteCookie(name){
+    document.cookie = name +'=; expires=Thu, 01 Jan 1970 00:00:00;';
+}
+
+// creates a Element for the repoData.
+function CreateRepoEl(repoData,toElement) {
+    // creates outer layer of repoCard
+    let section = document.createElement('section');
+    section.classList.add('col-6');
+    section.classList.add('outer');
+    
+    // adds class to inner div
+    let repository = document.createElement('div');
+    repository.classList.add('repo');
+
+    // creates h2 element with repo name.
+    let repoTitle = document.createElement('h2');
+    repoTitle.innerHTML = repoData.name;
+
+    // creates an p element with the repo description.
+    let repoDesc = document.createElement('p');
+    repoDesc.classList.add('desc');
+    repoDesc.innerHTML = repoData.desc;
+
+    //creates an p element with the last time the repo was updated.
+    let repoLUpdated = document.createElement('p');
+    repoLUpdated.classList.add('LatestUpdate');
+    repoLUpdated.innerHTML = repoData.lastUpdate;
+
+    //creates an p element with the latest commit description.
+    let repoLCommit = document.createElement('p');
+    repoLCommit.classList.add('lastCommit');
+    repoLCommit.innerHTML = repoData.lastCommit;
+
+    // appends all Elements
+    repository.appendChild(repoTitle);
+    repository.appendChild(repoDesc);
+    repository.appendChild(repoLUpdated);
+    repository.appendChild(repoLCommit);
+
+    section.appendChild(repository)
+    return toElement.appendChild(section);
+}
+
+function buildRepoDivs(cookieName){
+    const repoArray = getCookie(cookieName);
+    for (i=0; i != repoArray.length; i++) {
+        CreateRepoEl(repoArray[i],repoContainer);
+    }
+}
+
+// if the user cookie isnt set, start making a cookie for it.
+if(getCookie(user) == undefined) {
+    FetchRepoInfo().then((data) =>{
+        console.log(data);
+        createCookie(user,data,500);
+        buildRepoDivs(user);
+    })
+} else {
+    buildRepoDivs(user);
+}
+console.log(getCookie(user));
+
+
+
--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
-//gets all the user data from my github.\
+//gets all the user data from my github.
 const user = 'Powerboy2015';
 async function getRepos(user) {
     const response = await fetch('https://api.github.com/users/'+user+'/repos');
@@ -6,25 +6,19 @@ async function getRepos(user) {
 }
 
 async function getRepoCommits(user,repo){
-    const response = await fetch('https://api.github.com/repos/'+user+'/'+repo+'/commits');
+    const response = await fetch('https://api.github.com/repos/'+user+'/'+repo);
     return response.json()
 }
 
-function CreateRepoDiv(repoData,toElement) {
-    // creates outer layer of repocard
+function CreateRepoEl(repoData,toElement) {
+    // creates outer layer of repoCard
     let section = document.createElement('section');
     section.classList.add('col-6');
     section.classList.add('outer');
-
-
-
-    
     
     // adds class to inner div
     let repository = document.createElement('div');
     repository.classList.add('repo');
-
-
 
     // creates h2 element with repo name.
     let repoTitle = document.createElement('h2');
@@ -38,19 +32,14 @@ function CreateRepoDiv(repoData,toElement) {
     //creates an p element with the last time the repo was updated.
     let repoLUpdated = document.createElement('p');
     repoLUpdated.classList.add('LatestUpdate');
-    let cleanTime = repoData.updated_at.split('T');
+    let cleanTime = repoData.commit.committer.date.split('T');
     repoLUpdated.innerHTML = cleanTime[0]; 
 
     //creates an p element with the latest commit description.
     let repoLCommit = document.createElement('p');
     repoLCommit.classList.add('lastCommit');
-    getRepoCommits(user,repoData.name).then((commits) =>{
-        repoLCommit.innerHTML = commits[0].commit.message;
-    })
+    repoLCommit.innerHTml = repoData.commit.message;
 
-
-
-    
     // appends all Elements
     repository.appendChild(repoTitle);
     repository.appendChild(repoDesc);
@@ -63,17 +52,94 @@ function CreateRepoDiv(repoData,toElement) {
 
 const repoContainer = document.querySelector('div.repos')
 
-getRepos(user)
-    //puts all the repo's in data
-    .then((data) => {
-        console.log(data[0]);
-        data.forEach(repository => {
-            CreateRepoDiv(repository, repoContainer);
+// make an if statement to check if there's a cookie that exists with information about the github.
+const cookies = document.cookie;
+function newCookie(name, value) {
+    document.cookie = name+'='+value;
+}
+
+function deleteCookie(name){
+    document.cookie = name+'=; Max-age=-999999;';
+}
+
+// find a cookie with the appropriate name.
+function getCookie(name){
+    if(document.cookie != '') {
+            let cookies = document.cookie.split('; ')
+        .find((row) =>
+            row.startsWith(name+'='))
+        try{
+            let cookieSplit = cookies.split(/=(.*)/s);
+            return JSON.parse(cookieSplit[1]);
+        } catch{
+            return undefined;
+        }
+
+        // returns an error of cookies are empty.
+    } else{ 
+        console.error('empty cookies')};
+}
+
+// if the the user cookie is not set, fetch repository names from the user, and put them inside a cookie.
+if (getCookie(user) == undefined) {
+    const repos = []
+    getRepos(user)
+        .then((data) => {
+            data.forEach((repo) =>{
+                console.log(repo.name)
+                repos.push(repo.name);
+            });
+            newCookie(user,JSON.stringify(repos));
         });
-    })
+}
+
+//we retrieve the list of repos made by the user that has been set under the username of the user.
+// in the current scenario there are two of them: CV and ColoredSquareChanger.
+const repos = getCookie(user);
+console.log(repos);
 
 
-getRepoCommits(user,'CV')
-    .then((commits) =>{
-        console.log(commits[0].commit.message);
-})
+// we go through each of them to see if there is and cookie with it's name. If not, we retrieve the repo and put it in a cookie.
+// this is literally a foreach loop, but a usual foreach loop does not work here.
+for(let i=0; i != repos.length; i++){
+    let repo = repos[i];
+    if (getCookie(repo) == undefined) {
+        getRepoCommits(user,repo)
+            .then((commits) => {
+                let requiredInfo = {};
+                requiredInfo['Title'] = commits.name;
+                requiredInfo['Desc'] = commits.description;
+                requiredInfo['Update'] = commits.updated_at; 
+
+                console.log(commits);
+
+
+                newCookie(repo,JSON.stringify(requiredInfo));
+        }) 
+        console.log('sucess')
+    } else {
+        let repoData = getCookie(repo);
+        // CreateRepoEl(repoData,repoContainer);
+        console.log(repoData);
+
+    }
+}
+
+console.log(document.cookie);
+
+
+
+
+//         data.forEach(repository => {
+//             
+// })
+
+
+
+// for (let key in repos) {
+//     console.log(key);
+//     console.log(repos[key]);
+// }
+// console.log(document.cookie)
+
+
